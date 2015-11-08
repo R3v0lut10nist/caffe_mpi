77021c703477c2b0c3b59e3a3018b4935bd1dadc
bearpaw <platero.yang@gmail.com>
2015-11-08 19:53:59 +0800

    settings

---
+++ examples/Googlenet/up_BN_3000_wy/finetune.sh
@@ -0,0 +1,8 @@
+#/usr/local/bin/mpirun -np 2 ../../../build/tools/caffe train \
+#    --solver=./Google_solver.prototxt 2>&1 | tee G_D1_s1.txt
+postfix=`date +"%m-%d-%y"`
+
+/usr/bin/mpirun -np 4 /home/xyzeng/caffe-mpi_parallel_v3/build/install/bin/caffe train \
+--weights=./3k_bbox_bn.caffemodel  \
+--solver=./solver_freeze_bn.prototxt 2>&1 | tee log_$postfix.txt
+
