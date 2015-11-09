e6a1a312d2dbe72210f040213ef566db104071d6
bearpaw <platero.yang@gmail.com>
2015-11-09 21:47:07 +0800

    resume

---
+++ examples/Googlenet/BN_1000_allbox_pretrain_7215_8933/up_BN_1000_allbox_pretrain_7215_8933/resume.sh
@@ -0,0 +1,8 @@
+#/usr/local/bin/mpirun -np 2 ../../../build/tools/caffe train \
+#    --solver=./Google_solver.prototxt 2>&1 | tee G_D1_s1.txt
+postfix=`date +"%m-%d-%y"`
+
+/usr/bin/mpirun -np 4 /home/czhang/wyang/caffe_mpi/build/install/bin/caffe train \
+--snapshot=BN_box_pad16_freeze_bn_dropout_iter_74000.solverstate  \
+--solver=./solver_freeze_bn.prototxt 2>&1 | tee log-resume-$postfix.txt
+
